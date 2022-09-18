17af54be6d0a18e04d7544df7179b895c378c3db
dolgopyat <ajsdfhkljasbfgvm,d@bjvnb>
2022-09-18 20:25:28 +0300

    Дз

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,7 @@
 public class Main {
     public static void main(String[] args) {
+        // Совершенно запутался. Разбор домашек в Skypro от 17.09 не из нашего курса.
+        // Меня выкинуло из Mattermost, не знаю что там нужно ввести чтоб опять всё заработало.
+        // Моя почта: dolgopyatov.andrey@mail.ru
     }
 }
